d547f9bcf34a12dc4e76a95de18743def29b4f98
Ali AslRousta <aslrousta@gmail.com>
2019-03-27 11:17:54 +0430

    Random bytes and string

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aslrousta/rand
+
+go 1.12
+
+require github.com/aslrousta/errors v0.2.0
---
+++ rand.go
@@ -0,0 +1,70 @@
+package rand
+
+import (
+	"crypto/rand"
+	"errors"
+	"strings"
+
+	xerrors "github.com/aslrousta/errors"
+)
+
+// Charset is the data type for random alphabet charset.
+type Charset int
+
+// List of charsets.
+const (
+	Uppercase = 1 << iota
+	Lowercase
+	Digit
+)
+
+// RandomBytes generates a slice of random bytes.
+func RandomBytes(n int) ([]byte, error) {
+	if n < 1 {
+		return nil, errors.New("invalid random bytes length")
+	}
+
+	data := make([]byte, n)
+	if _, err := rand.Read(data); err != nil {
+		return nil, xerrors.Wrap(err, "random bytes failed")
+	}
+
+	return data, nil
+}
+
+// RandomString generates a random string.
+func RandomString(n int, charset Charset) (string, error) {
+	alphabet := []byte(makeCharset(charset))
+	alphaLen := len(alphabet)
+	if alphaLen == 0 {
+		return "", errors.New("charset is empty")
+	}
+
+	bytes, err := RandomBytes(n)
+	if err != nil {
+		return "", xerrors.Wrap(err, "random string failed")
+	}
+
+	var sb strings.Builder
+	for _, b := range bytes {
+		sb.WriteByte(alphabet[int(b)%alphaLen])
+	}
+
+	return sb.String(), nil
+}
+
+func makeCharset(charset Charset) string {
+	var sb strings.Builder
+
+	if charset&Uppercase > 0 {
+		sb.WriteString("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	}
+	if charset&Lowercase > 0 {
+		sb.WriteString("abcdefghijklmnopqrstuvwxyz")
+	}
+	if charset&Digit > 0 {
+		sb.WriteString("0123456789")
+	}
+
+	return sb.String()
+}
